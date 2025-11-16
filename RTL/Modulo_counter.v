2bfdef0ca91905c8900d2499c4ffe61648b99725
ABHILASH MADDINENI <214151262+MrAbhi19@users.noreply.github.com>
2025-11-16 17:08:49 +0530

    Update Modulo_counter.v

--- RTL/Modulo_counter.v
+++ RTL/Modulo_counter.v
@@ -1,15 +1,15 @@
 module Modulo_counter #(
-  parameter DIVISOR = 10
+  parameter SIZE = 10
 )(
   input wire clk,
   input wire reset_n,
-  output reg [$clog2(DIVISOR)-1:0] count
+  output reg [$clog2(SIZE)-1:0] count
 );
 
   always @(posedge clk or negedge reset_n) begin
     if (!reset_n)
       count <= 0;
-    else if (count == DIVISOR-1)
+    else if (count == SIZE-1)
       count <= 0;
     else
       count <= count + 1;
